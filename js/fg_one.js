9e5910513e6d51ab80d56c0921f9562145e1ddea
rpagitz <rpagitz@a1.net>
2014-06-25 14:14:05 +0200

    output format
    fix

--- js/fg_one.js
+++ js/fg_one.js
@@ -1,7 +1,16 @@
 var FGONE = (function(module){
 
 module.Doit = function(resultObject){
-resultObject.prepend(Date()+'<br/>');
+var d = new Date();
+var theyear=d.getFullYear();
+var themonth=d.getMonth()+1;
+var thetoday=d.getDate();
+
+var t = d.getTime();
+var thehours=d.getHours();
+var theminutes=d.getMinutes();
+var theseconds=d.getSeconds();
+resultObject.prepend(theyear+'/'+themonth+'/'+thetoday+' - '+ thehours+':'+theminutes+':'+theseconds+'<br/>');
 }
 
 return module;
